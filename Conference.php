3e42808247b34dadf9ac26e1c49965d7eaeeebba
waghdude <waghdude@users.noreply.github.com>
2014-07-13 00:23:51 -0700

    final deployed solution
    added php files related to depoloyment. The Conference.php belongs at a
    different part of the tree. Since the deployed version uses index.php,
    the old index.html has moved to start.html

---
+++ Conference.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+
+
+<html>
+
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<title>Region 8 Sai Centers</title>
+       
+<link rel="stylesheet" type="text/css" href="css/style.css" />	
+		<style type="text/css" media="screen"><!--
+
+p.news { text-align:justify; text-justify:inter-word; font-size:14px; line-height:19px; letter-spacing:.03em; padding-bottom:9px; padding-left:9px; }
+
+a:link.pdfs { font-size:15px; line-height:27px; letter-spacing:.03em; color:#F30; padding-left:3px; padding-right:3px; text-decoration:none; }
+a:active.pdfs { color:#F30; text-decoration:underline; }
+a:hover.pdfs { color:#F60; text-decoration:underline; }
+a:visited.pdfs { color:#F93; text-decoration:none;  }
+--></style>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
+
+	</head>
+
+	<body><a name="top"></a>
+    <?php include("header.php");?>
+   	<div id="content" style="max-width:100%;">
+		<div id="shadow" style="width:970px;">
+		<table style="border-collapse: collapse; width:900px;" align="center">
+          <tr>
+            <td colspan="2" align="center" style="text-align:center; padding:27px 0 18px 0;">	
+				<span style="font-size: 25px; line-height: 21px; font-family: arial, helvetica;"><b>2014 Sathya Sai Baba Conference of the Pacific Region South</b><br /><span style="font-size: 18px; line-height: 21px">Duarte Performing Arts Center, Saturday, August 30, 2014 and Sunday, August 31, 2014</span><br />
+					<span style="font-size: 12px; line-height: 21px; color: #999999;"><a class="pdfs" href="#RegistrationForm" ><b>Online Registration</b></a>  &nbsp;|&nbsp;
+						<a class="pdfs"href="pdf/ConferenceRegistration14.pdf" target="_blank"><b>Download Registration Form</b></a> &nbsp;|&nbsp;
+						<a class="pdfs" href="pdf/ConferenceInfo14.pdf" target="_blank"><b>Invitation</b></a> &nbsp;|&nbsp;
+						<a class="pdfs" href="pdf/ConferenceProgram14.pdf" target="_blank"><b>Program</b></a> &nbsp;|&nbsp;
+						<a class="pdfs" href="https://www.google.com/maps/place/1401+Highland+Ave,+Duarte,+CA+91010/@34.0465184,-117.988764,11z/data=!4m2!3m1!1s0x80c2d8ff9daf64af:0xbb45ab5a5b4642cd" target="_blank"><b>Directions</b></a>
+					</span>
+		</span>
+			</td>
+		  </tr>
+		  
+		  <tr>
+			<td width="180" valign="top" align="center" style="border-right:thin solid rgba(153,153,153,1); /* IE9 SVG, needs conditional override of 'filter' to 'none' */
+
+				background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIxMDAlIiB5Mj0iMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI2ZmZmZmZiIgc3RvcC1vcGFjaXR5PSIwIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNlYWVhZWEiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
+
+				background: -moz-linear-gradient(left,  rgba(255,255,255,0) 0%, rgba(234,234,234,1) 100%); /* FF3.6+ */
+
+				background: -webkit-gradient(linear, left top, right top, color-stop(0%,rgba(255,255,255,0)), color-stop(100%,rgba(234,234,234,1))); /* Chrome,Safari4+ */
+
+				background: -webkit-linear-gradient(left,  rgba(255,255,255,0) 0%,rgba(234,234,234,1) 100%); /* Chrome10+,Safari5.1+ */
+
+				background: -o-linear-gradient(left,  rgba(255,255,255,0) 0%,rgba(234,234,234,1) 100%); /* Opera 11.10+ */
+
+				background: -ms-linear-gradient(left,  rgba(255,255,255,0) 0%,rgba(234,234,234,1) 100%); /* IE10+ */
+
+				background: linear-gradient(to right,  rgba(255,255,255,0) 0%,rgba(234,234,234,1) 100%); /* W3C */
+
+				filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#eaeaea',GradientType=1 ); /* IE6-8 */
+
+				;">
+				<p style="padding-left: 5px; padding-right: 1px; padding-top:14px; text-align:right;">
+				
+				<b>Where:</b><br />Duarte Performing<br>Arts Center<br />
+				1401 Highland Ave.<br />
+				Duarte, CA, 91010<br />
+				<br />
+				<a class="pdfs" style="padding-left:0px;" href="https://www.google.com/maps/place/1401+Highland+Ave/@34.0465184,-117.988764,11z/data=!4m2!3m1!1s0x80c2d8ff9daf64af:0xbb45ab5a5b4642cd" target="_blank">Driving Directions</a>
+				<br />
+				<br />
+				<b>Contact:</b><br />Bruce Bouché<br>
+				<a class="pdfs" style="padding-left:0px;" href="mailto:bruce@brucebouche.com">bruce@brucebouche.com</a><br />
+				phone: 805-807-3309
+				<br />
+				<br />
+				</p>
+			
+			</td>
+			
+			<td align="center">
+			<br />
+			
+			<div id="retreatWriteup">
+
+<p class="news">
+<b>Sai Ram!</b> You are cordially invited to attend the Annual Sathya Sai
+Baba Conference of Region 8 on Saturday, Aug 30, 2014 and
+Sunday, Aug 31, 2014.
+</p>
+                       
+
+<p class="news" style="text-align:center;"><b>This year's conference theme is "<i>Living in His Omnipresence.</i>"</b>
+</p>
+
+
+<p class="news"><img src="images/dpac.jpg" style="display:block; float:right; padding-left:9px; padding-bottom:9px; margin-right:-9px; border:0;" >
+The regional conference will be held this year at the Duarte Performing Arts Center,
+1401 Highland Avenue, in Duarte, California 91010. The conference registration fee for
+adults (age 19 and over) is $20 for both Saturday and Sunday or $10 for one day only.
+For children and youth (18 years of age and under) registration is free of charge.
+</p>
+                       
+
+<p class="news">We are blessed to have three very special guest speakers this year: 
+<b>Bob Bozzani, Sathya Atreyam, and Srikanth Vaidyanathan</b>.
+</p>
+
+<p class="news"><b>Bob Bozzani </b> 
+graduated from Occidental College in the northeast Los Angeles
+community of Eagle Rock. Following his service as an officer in the U.S. Navy, he
+joined the family automobile business that was established in 1911. He served as
+President of the Monrovia Chamber of Commerce and as President of the Porsche-Audi
+Dealers Association of Southern California. He was a member of the Board of Directors
+of the YMCA, and he was also a member of the Rotary Club.
+</p>
+
+<p class="news">Bob is a long-time Sai Devotee. He first came to know about Swami in 1972, and his
+first trip to see Swami was in 1974. He and his wife Barbara traveled to Prasanthi
+Nilayam many times to be with Swami. Over the past 40 years he served in many roles
+in the Sai Organization. Bob was President of the Sathya Sai Baba Society and Book
+Center of America for over 25 years.
+</p>
+                       
+
+<p class="news"><b>Sathya Atreyam's </b> 
+parents journeyed from Shirdi to Parthi in the late 1970s. Sathya's
+mother, through her role as the Mumbai Ladies Seva Dal convenor for more than 20
+years, is his main inspiration. Sathya's father served as an honorary professor in the
+Department of Chemistry in Sathya Sai University after retiring from his career as a
+nuclear scientist. Sathya's younger sister is an Anantapur alumnus.
+</p>
+
+
+<p class="news">Sathya has been serving in the Sathya Sai Organization in various capacities in India
+and in the USA. After completing his Diploma through a 9-year Bal Vikas program, he
+served in different wings of the All India Sri Sathya Sai Organization. Sathya is currently
+a member of the USA Sathya Sai Central Council and serves as the President of the
+Northeast Region, Region 1, USA.
+</p>
+                       
+
+<p class="news">Sathya has worked for more than 20 years in the telecommunication industry in various
+capacities. He is currently working at IDC, a leading market research firm, as an
+Industry Analyst tracking disruptive inventions in the field of communications. He plans
+to shortly launch a telecom startup based on the principles of what he calls SAI
+Entrepreneurship.
+
+</p>
+
+
+<p class="news"><b>Srikanth Vaidyanathan </b> studied in Swami's School and University from 1983 to 1998.
+He was personally guided by Swami in public speaking, dramatics and music. Srikanth
+has spoken in Swami's presence a number of times and continues to speak in many
+retreats across the US. He currently serves as the Regional Devotion Coordinator for
+the Northeast Region, Region 1, and lives in the Boston Area.
+</p>
+                       
+
+<p class="news">In addition to these three very special Guest Speakers, the conference program
+includes a Young Adult presentation, a Children's presentation, bhajans, Sai sharing by
+selected speakers, and a workshop led by Bob Bozanni. Please plan to attend this very
+special conference.
+</p>
+
+
+<p class="news">SSE Program: Parents of SSE-age children aged 6 and above are urged to register for
+the conference for both Saturday and Sunday. The Sai Spiritual Education program this
+year will focus on spiritual, fun-filled sessions in the morning and afternoon on both
+Saturday and Sunday. These sessions will lead to a children's presentation to the
+general assembly on Sunday afternoon. Your child's attendance in the SSE sessions on
+both days will allow him/her to participate in the children's presentation on Sunday
+afternoon.
+</p>
+                       
+
+<p class="news">Parents can enjoy key conference talks while their children are engaged in value-based
+activities with other children of their own age. More details will be forwarded to parents
+as the date of the conference draws closer.
+</p>
+
+<p class="news"><span style="color:rgba(255,51,0,1);"><b>REGISTRATION DEADLINE is Saturday, August 16, 2014.</b></span>
+</p>
+                       
+
+<p class="news">Please register before the deadline in order to help the organizers plan for the event.
+You may register and pay online or by mail. Register and pay online by credit card at
+the Region 8 website (http://www.region8saicenters.org/Conference.php.) Register by
+mail by obtaining a registration form from your Sai Center or from the Region 8 website,
+completing the form, and mailing it along with a check for the registration fee to:
+</p>
+
+
+<p class="news">Bruce Bouché<br>
+2225 Los Encinos Road<br>
+Ojai, California 93023
+</p>
+                       
+
+<p class="news">If you have any questions or need additional information, please call or email the
+Conference Registrar, Bruce Bouché, at <a href="mailto:bruce@brucebouche.com">bruce@brucebouche.com</a> or 805-807-3309.
+
+</p>
+  
+
+<p class="news"><b>DINING INFORMATION:</b> Lunch, tea and snacks will be provided on Saturday and
+Sunday at no cost to registrants. In addition, on both Saturday and Sunday at the close
+of the day's conference program, boxed prasadam will be provided to all participants.
+</p>
+
+<a name="RegistrationForm"></a>
+<p class="news" style="font-size:27px; color:#1a6329; padding-top:54px;padding-left:27px;"><b>Online Registration Form:</b>
+	<a class="pdfs" href="/Conference/2014/">click here</a>
+</p>
+
+        
+<p class="news" style="width:720px;"><b>ACCOMMODATION INFORMATION: HOTEL/MOTEL INFORMATION:</b><br>
+Those who wish to have overnight accommodations should make arrangements at a nearby hotel/motel. Several
+options are listed below (call the hotel directly for rates and current information) and additional options can be found at www.tripadvisor.com or hotel booking websites. Note that the information provided below may have changed since the date this
+document was prepared. Please check all information before booking a hotel.
+<br><br>
+
+<b>SOME NEARBY HOTELS:</b><br><br>
+
+<b>Days Inn Near City of Hope</b> (0.8 miles from venue)<br>
+<a class="pdfs" href="www.daysinn.com/hotels/california/duarte/days-inn-near-city-of-hope/hotel-overview" target="_blank">www.daysinn.com</a><br>
+Pets allowed, smoking allowed In some rooms, rooms open to exterior corridors, free
+continental breakfast, from $70 plus tax<br>
+1533 E Huntington Drive<br>
+Duarte, CA 91010<br>
+Tel: (626) 303-4544<br><br><br>
+
+<b>Quality Inn Near City of Hope</b> (2.1 miles from venue)<br>
+<a class="pdfs" href="www.qualityinn.com/hotel-monrovia-california-CA337" target="_blank">www.qualityinn.com</a><br>
+No pets allowed, smoking allowed in some rooms, rooms open to exterior corridors, free
+continental breakfast, from $80 plus tax.<br>
+1125 E Huntington Drive<br>
+Monrovia, CA 91016<br>
+Tel: (626) 358-0430<br><br><br>
+
+<b>Hampton Inn Los Angeles/Arcadia/Pasadena</b> (4.0 miles from venue)<br>
+<a class="pdfs" href="http://hamptoninn3.hilton.com/en/hotels/california/hampton-inn-los-angeles-arcadiapasadena-LAXACHX/index.html" target="_blank">hamptoninn3.hilton.com</a><br>
+No smoking or pets allowed, free hot breakfast, rooms open to interior hallway, from
+$100 per night plus tax.<br>
+311 E Huntington Dr.<br>
+Arcadia, CA 91006<br>
+Tel: (626) 574-5600<br><br><br>
+
+<b>Double Tree by Hilton</b> (3.2 miles from venue)<br>
+<a class="pdfs" href="http://doubletree3.hilton.com/en/hotels/california/doubletree-by-hilton-hotel-monrovia-pasadenaarea-LAXMVDT/index.html" target="_blank">doubletree3.hilton.com</a><br>
+Non-smoking, small pets allowed, rooms open to interior hallway, bed and full breakfast
+package from $143 plus tax.<br>
+924 W Huntington Drive<br>
+Monrovia, CA 91016<br>
+Tel: (626) 357-1900<br><br><br>
+
+<b>Chariot Inn</b> (16.7 miles from venue)<br>
+<a class="pdfs" href="www.chariotinn.com" target="_blank">www.chariotinn.com</a><br>
+Smoking allowed in some rooms, rooms open to exterior corridors, continental breakfast
+included, from $95 plus tax.<br>
+1118 E. Colorado St.<br>
+Glendale, California 91205<br>
+Tel: (818) 507-9600<br><br><br>
+
+</p>
+        
+<div id="subFooter" style="width:720px; margin:0 auto 90px auto;">       
+<hr width="72%">
+
+	<span style="font-size: 12px; line-height: 21px; color: #999999;"><a class="pdfs" href="#RegistrationForm" ><b>Online Registration</b></a>  &nbsp;|&nbsp;
+		<a class="pdfs"href="pdf/ConferenceRegistration14.pdf" target="_blank"><b>Download Registration Form</b></a> &nbsp;|&nbsp;
+		<a class="pdfs" href="pdf/ConferenceInfo14.pdf" target="_blank"><b>Invitation</b></a> &nbsp;|&nbsp;
+		<a class="pdfs" href="pdf/ConferenceProgram14.pdf" target="_blank"><b>Program</b></a> &nbsp;|&nbsp;
+		<a class="pdfs" href="https://www.google.com/maps/place/1401+Highland+Ave,+Duarte,+CA+91010/@34.0465184,-117.988764,11z/data=!4m2!3m1!1s0x80c2d8ff9daf64af:0xbb45ab5a5b4642cd" target="_blank"><b>Directions</b></a>
+	</span>
+		  
+<br><br>
+
+<a class="pdfs" href="#top"><b>Return to top of the page</b></a>
+</div>		   
+	
+</div>
+	
+        </td>
+    
+    </tr>                           
+				
+	</table>
+    
+	</div>
+    <?php include("footer2.php"); ?>
+   	</div>
+	
+	</body>
+
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<title>Region 8 Sai Centers - Conference Registration</title>
+   
+<link rel="stylesheet" type="text/css" href="/css/style.css" />	
+	<style type="text/css" media="screen"></style>
+</head>
+
+<body><a name="top"></a>
+	<?php include("../../header.php"); ?>
+
+   	<div id="content" style="max-width:100%;">
+		<div id="shadow" style="width:900px;">
+
+			<iframe width="900" height="1280" src="start.html" seamless></iframe> 
+		</div>
+	</div>
+
+</body>
+
+</html>
+
+
+
